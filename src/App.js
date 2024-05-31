bb6cdb6d4f9a14ce051f6028a01704bf8e59a69b
EphriamHab <ephremhabtamu2015@gmail.com>
2024-05-31 13:41:57 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React, { useEffect, useState } from 'react';
-
-import './App.css';
-
-function App() {
-  
-  
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
-export default App;
+
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        💼 You have X item in your list, and you have already packed X (X%)
+      </em>
+    </footer>
+  );
+}
